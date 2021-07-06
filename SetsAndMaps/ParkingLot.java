627630a1e21e349ba7db35b5fe4fd0262e462225
Denislav Dobrevski <78070754+DenislavDobrevski@users.noreply.github.com>
2021-07-07 01:20:14 +0300

    Add files via upload

---
+++ SetsAndMaps/ParkingLot.java
@@ -0,0 +1,26 @@
+package SetsAndMaps;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ParkingLot {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        Set<String> parkingLot = new LinkedHashSet<>();
+        String input;
+        while (!"END".equals(input = scan.nextLine())) {
+            String[] inputData = input.split(", ");
+            if (inputData[0].equals("IN")) {
+                parkingLot.add(inputData[1]);
+            } else {
+                parkingLot.remove(inputData[1]);
+            }
+        }
+        System.out.print(parkingLot.isEmpty()
+                ? "Parking Lot is Empty"
+                : parkingLot.stream().collect(Collectors.joining(System.lineSeparator())));
+    }
+}
+
